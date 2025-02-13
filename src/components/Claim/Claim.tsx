f8afda7dde7b558b461fd0cf25e4c10dc8569fa9
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-02-14 01:58:37 +0800

    changed message

--- src/components/Claim/Claim.tsx
+++ src/components/Claim/Claim.tsx
@@ -1,69 +1,69 @@
-import { Button } from "@/components/ui/button";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import { useToast } from "@/hooks/use-toast";
+import { Button } from '@/components/ui/button'
+import { useCookies } from 'react-cookie'
+import axios from 'axios'
+import { useToast } from '@/hooks/use-toast'
 
 export const Claim = () => {
-  const URLLINK = "http://member-validation.app.dlsu-lscs.org";
-  const URLLINK2 = "http://tomo-scanner.app.dlsu-lscs.org";
-  const [cookies, setCookie] = useCookies(["currentUser"]);
-  const { toast } = useToast();
+  const URLLINK = 'http://member-validation.app.dlsu-lscs.org'
+  const URLLINK2 = 'http://tomo-scanner.app.dlsu-lscs.org'
+  const [cookies, setCookie] = useCookies(['currentUser'])
+  const { toast } = useToast()
 
   const fetchData = async () => {
     try {
       const response = await axios.get(
         `${URLLINK2}/status?studentId=${cookies.currentUser?.member_details?.id}`,
         {
-          headers: { "Content-Type": "application/json" },
+          headers: { 'Content-Type': 'application/json' },
         }
-      );
-      setCookie("currentUser", response.data, { path: "/", maxAge: 60 });
+      )
+      setCookie('currentUser', response.data, { path: '/', maxAge: 60 })
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.error(error.response?.data?.message || "An error occurred");
+        console.error(error.response?.data?.message || 'An error occurred')
       } else {
-        console.error(error);
+        console.error(error)
       }
     }
-  };
+  }
 
   const postData = async () => {
     try {
       const response = await axios.post(
         `${URLLINK}/claim`,
         { studentId: Number(cookies.currentUser?.member_details?.id) },
-        { headers: { "Content-Type": "application/json" } }
-      );
+        { headers: { 'Content-Type': 'application/json' } }
+      )
 
-      const message = response.data.message;
-      const fullName = cookies.currentUser?.member_details?.full_name;
-      const toastMessage = message.includes("already claimed")
-        ? `${fullName} has already claimed the Promo`
-        : `${fullName} has claimed the Promo`;
+      const message = response.data.message
+      const fullName = cookies.currentUser?.member_details?.full_name
+      const toastMessage = message.includes('already claimed')
+        ? `${fullName} has already claimed`
+        : `${fullName} has claimed`
 
-      if (!message.includes("already claimed")) {
+      if (!message.includes('already claimed')) {
         toast({
           title: toastMessage,
-          className: "text-white bg-black rounded-lg border-2",
-        });
-        fetchData();
+          className: 'text-white bg-black rounded-lg border-2',
+        })
+        fetchData()
       } else {
         toast({
           title: toastMessage,
-          className: "text-white bg-black rounded-lg border-2",
-        });
-        fetchData();
+          className: 'text-white bg-black rounded-lg border-2',
+        })
+        fetchData()
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
         toast({
-          title: error.response?.data?.message || "Request failed",
-          className: "text-white bg-black rounded-lg border-2",
-        });
+          title: error.response?.data?.message || 'Request failed',
+          className: 'text-white bg-black rounded-lg border-2',
+        })
       }
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
     <Button
@@ -71,9 +71,9 @@ export const Claim = () => {
       onClick={postData}
       className="text-white bg-black"
     >
-      Claim Coffee
+      Claim Scramble
     </Button>
-  );
-};
+  )
+}
 
-export default Claim;
+export default Claim
